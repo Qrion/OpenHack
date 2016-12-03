496587c06eca2b00f23c510204cbb103c50d005c
MichaelArenander <michaelarenander@Michaels-MacBook-Pro.local>
2016-12-03 16:15:41 +0100

    tiny commit

---
+++ App/data/login.js
@@ -0,0 +1,19 @@
+$(document).ready(function()
+{
+    
+    $("#signIn").click(function(e)
+    {
+        var email = document.getElementById("#email");
+        var pasword = document.getElementById("#password");
+        
+        if(email === "hej123" && password === "1")
+        {
+            
+        }
+        else
+        {
+            
+        }
+    });
+    
+}
