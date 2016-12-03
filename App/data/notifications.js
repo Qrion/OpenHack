ed0263eeb6f6c77770e4175e27623a169fd8ef3c
Joakim Loxdal <joakim.loxdal@ibm.com>
2016-12-03 20:06:05 +0100

    nice timers

--- App/data/notifications.js
+++ App/data/notifications.js
@@ -1,32 +1,32 @@
 var notifications = [{
     'userid':3,
     'title':'Du har erbjudit dig att hjälpa Reshin',
-    'time': '2016-08-29 20:09',
+    'time': '22:40',
     'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
 },
 {    'userid':3,
     'title':'Du har fått en högre rang',
-    'time': '2016-08-29 20:09',
-    'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
+    'time': '21:00',
+    'text':'Grattis! Du har blivit upphöjd till rangen grön'
 },                    
 {
     'userid':3,
     'title':'Någon har skrivit något till dig',
-    'time': '2016-08-29 20:09',
+    'time': '20:01',
     'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
 
 },
 {
     'userid':3,
     'title':'Tack för att du hjälpte Jona!',
-    'time': '2016-08-29 20:09',
+    'time': '07:00',
     'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
 
 },
 {
     'userid':3,
     'title':'Du har erbjudit dig att hjälpa Jona',
-    'time': '2016-08-29 20:09',
+    'time': 'Igår',
     'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
 
 }];
