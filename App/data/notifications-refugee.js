ecf78b66137afc11efd424bd5b7b25d16626e668
Joakim Loxdal <joakim.loxdal@ibm.com>
2016-12-03 20:11:49 +0100

    notifications refugee

---
+++ App/data/notifications-refugee.js
@@ -0,0 +1,32 @@
+var notifications = [{
+    'userid':3,
+    'title':'Armin just volonteered to help you',
+    'time': '22:40',
+    'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
+},
+{    'userid':3,
+    'title':'Red Cross is hosting events in your area',
+    'time': '21:00',
+    'text':'Grattis! Du har blivit upphöjd till rangen grön'
+},                    
+{
+    'userid':3,
+    'title':'Jonas just helped you, how was it?',
+    'time': '20:01',
+    'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
+
+},
+{
+    'userid':3,
+    'title':'Jonas just requested to help you',
+    'time': '20:01',
+    'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
+
+},
+{
+    'userid':3,
+    'title':'You just requested help',
+    'time': '20:01',
+    'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
+
+}];
