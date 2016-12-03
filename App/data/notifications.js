82e8c7dbeccf59b0ee23eba2617cd0f67fbefc85
Joakim Loxdal <joakim.loxdal@ibm.com>
2016-12-03 18:04:35 +0100

    notifications

--- App/data/notifications.js
+++ App/data/notifications.js
@@ -5,13 +5,27 @@ var notifications = [{
     'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
 },
 {    'userid':3,
-    'title':'Du har erbjudit dig att hjälpa Sara',
+    'title':'Du har fått en högre rang',
     'time': '2016-08-29 20:09',
     'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
 },                    
 {
     'userid':3,
-    'title':'Du har erbjudit dig att hjälpa Sven',
+    'title':'Någon har skrivit något till dig',
+    'time': '2016-08-29 20:09',
+    'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
+
+},
+{
+    'userid':3,
+    'title':'Tack för att du hjälpte Jona!',
+    'time': '2016-08-29 20:09',
+    'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
+
+},
+{
+    'userid':3,
+    'title':'Du har erbjudit dig att hjälpa Jona',
     'time': '2016-08-29 20:09',
     'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
 
