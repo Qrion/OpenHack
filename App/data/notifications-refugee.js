9227570b0e2eea9cadb71c313a2184d001b5d548
Joakim Loxdal <joakim.loxdal@ibm.com>
2016-12-04 07:21:37 +0100

    fixes

--- App/data/notifications-refugee.js
+++ App/data/notifications-refugee.js
@@ -1,26 +1,30 @@
 var notifications = [{
     'userid':3,
-    'title':'You just requested help',
+    'title':'You just requested a service',
     'time': '00:01',
-    'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
+    'text':'You just requested a service: "Läxhjälp".'
 
 },{
     'userid':3,
     'title':'Armin just volonteered to help you',
     'time': '22:40',
-    'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
+    'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126',
+    'type':'response'
+    
 },
 {    'userid':3,
     'title':'Red Cross is hosting events in your area',
     'time': '21:00',
     'text':'Red Cross is hosting the event: T-shirts for Impact. Follow this link for more information:',
-    'link':'https://www.icrc.org/en'
+    'link':'https://www.icrc.org/en',
+    'link-title':'find it here!'
 },                    
 {
     'userid':3,
     'title':'Jonas just helped you, how was it?',
     'time': '20:01',
-    'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
+    'text':'Please rate your experience:',
+    'type':'rate'
 
 },
 {
@@ -32,7 +36,7 @@ var notifications = [{
 },
 {
     'userid':3,
-    'title':'You just requested help',
+    'title':'You just requested a service',
     'time': '20:01',
     'text':'Title: Help with homework. Description: I need help with my homework. Very happy for every bit of help! Time: Any time. Phone number: 0725129126'
 
